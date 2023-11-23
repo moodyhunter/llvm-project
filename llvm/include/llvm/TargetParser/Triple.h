eb84733801dabe71e7e362e8047566180db0d226
Moody Liu <mooodyhunter@outlook.com>
2023-11-23 15:12:43 +0000

    target: add MOS target

--- llvm/include/llvm/TargetParser/Triple.h
+++ llvm/include/llvm/TargetParser/Triple.h
@@ -225,6 +225,7 @@ public:
     Emscripten,
     ShaderModel, // DirectX ShaderModel
     LiteOS,
+    MOS,        // MOS
     Serenity,
     Vulkan, // Vulkan SPIR-V
     LastOSType = Vulkan
@@ -661,6 +662,11 @@ public:
   /// Tests whether the OS is Linux.
   bool isOSLinux() const {
     return getOS() == Triple::Linux;
+  }
+
+  /// Tests whether the OS is MOS.
+  bool isOSMOS() const {
+    return getOS() == Triple::MOS;
   }
 
   /// Tests whether the OS is kFreeBSD.
